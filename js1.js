0e0089afcd6caf242659cc216cea78953c178fae
Jyothika-Jayaprakash <85836260+Jyothika-Jayaprakash@users.noreply.github.com>
2021-06-29 13:05:54 +0530

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,13 @@
+const sign_in_btn=document.querySelector('#sign-in-button');
+const sign_in_btn=document.querySelector('#sign-up-button');
+const sign_in_btn=document.querySelector('.container');
+
+
+sign_up_btn.addEventListner('click',() =>{
+    container.classList.add('sign-up-mode');
+
+});
+
+sign_in_btn.addEventListner('click',() =>{
+    container.classList.add('sign-up-mode');
+});
